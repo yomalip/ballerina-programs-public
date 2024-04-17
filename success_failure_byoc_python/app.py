f2e4d1d8bcbbb4b60008ce21e737f26127332166
Nilushan Costa <nilushan@wso2.com>
2024-04-17 11:36:59 +0530

    Add python byoc app

---
+++ success_failure_byoc_python/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Endpoint for success (returns HTTP 200)
+@app.route('/success')
+def success():
+    print("Request received at /success endpoint")
+    return jsonify(message='Success'), 200
+
+# Endpoint for failure (returns HTTP 500)
+@app.route('/failure')
+def failure():
+    print("Request received at /failure endpoint")
+    return jsonify(message='Failure'), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Run the app on localhost port 5000
